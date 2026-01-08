8c0cc1f2c1648b36ecdef457975caf6cc6fa1a57
Arkodev Mukherjee <arkodev.programmer@gmail.com>
2026-01-09 00:42:46 +0530

    Update the code of the repo

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes,Route } from 'react-router-dom'
+import Home from "./Home.jsx"
+import About from "./About.jsx"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    Hello
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+    
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router'
+const basePath = window.location.pathname.replace(/\/$/, ''); // strips trailing slash
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <BrowserRouter basename={process.env.VITE_BASE_PATH}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>,
 )
